985e3674555978155b2d123fd03693c84f0e8b38
delmarNK <delmarjinku3@gmail.com>
2024-01-26 23:07:40 +0800

    created a server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+//require the .env file to express to make connection between the 2 files
+require('dotenv').config()
+// to get variable from .env we use process.env.name
+const PORT = process.env.PORT
+const app = express()
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
+})
