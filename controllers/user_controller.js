ff427226c0a64cecad5fa2a36aba80da2286edd5
delmarNK <delmarjinku3@gmail.com>
2024-01-28 22:29:32 +0800

    added controllers

---
+++ controllers/user_controller.js
@@ -0,0 +1,27 @@
+//We create the user controller to do CRUD on data coming from the user model 
+const express = require('express')
+const router = express.Router()
+
+const User = require('../models/Post')
+
+router.use(express.urlencoded({extended: false}))
+
+//create CRUD to use data saved in the database
+//Read route
+router.get('/:id', async(req,res)=>{
+    const data = await User.findById(req.params.id)
+    res.send(data)
+})
+
+//delete route
+router.delete('/:id', async(req,res)=>{
+    const data = await User.findByIdAndDelete(req.params.id)
+    res.send(data)
+})
+//update
+router.put('/:id', async(req,res)=>{
+    const data = await User.findByIdAndUpdate(req.params.id, req.body)
+    res.send(data)
+})
+
+module.exports = router
