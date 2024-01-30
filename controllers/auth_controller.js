ab41cc16af00d39b1b5dd11f462f92f8bee89f81
delmarNK <delmarjinku3@gmail.com>
2024-01-30 21:42:03 +0800

    updated the feed.js file

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -6,4 +6,23 @@ router.get('/login', (req,res)=>{
     res.render('login.ejs')
 })
 
+router.get('/register', (req,res)=>{
+    res.render('register.ejs')
+})
+
+router.post('/register', async(req,res)=>{
+    const userExists = await User.exists({email: req.body.email})
+
+    if(userExists){
+        res.send('This email is already used, please <a href="/login">login Here</a>')
+    }
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(req.body.password, salt)
+
+    req.body.password = hash
+    const newUser = await this.unsubscribe.create(req.body)
+
+    res.redirect('/login')
+})
+
 module.exports = router
--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -2,5 +2,8 @@ const express = require('express')
 const router = express.Router
 const Post = require('../models/Post')
 
+router.use(express.urlencoded({extended: false}))
+
+
 
 
