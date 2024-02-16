9a9c3b166c9d83afd4462dfba02404132f4da00f
delmarNK <delmarjinku3@gmail.com>
2024-02-16 13:11:10 +0800

    upadated the chat controller and chat.ejs

--- controllers/chat_controller.js
+++ controllers/chat_controller.js
@@ -6,18 +6,18 @@ const Message = require('../models/Message')
 router.use(express.json())
 
 router.get('/:id', async(req,res)=>{
-    let chat = await Chat.findOne().all('users', [req.params.id, req.session.currentUser.id])
+    let chat = await Chat.findOne().all('users', [req.params.id, req.session.currentUser.id]).populate('messages')
     if(!chat){
         chat = await Chat.create({users: [req.params.id, req.session.currentUser.id]})
     }
     const profile = await User.findById(req.params.id)
-    res.render('chat.ejs', {profile})
+    res.render('chat.ejs', {profile, chat})
 })
 
 router.post('/:id', async(req,res)=>{
+    req.body['chat'] = req.params.id
     req.body['user'] = req.session.currentUser.id
-    await Message.create(req.body)
-    res.redirect('/')
+    const message = await Message.create(req.body)
 })
 
 router.delete('/:id/delete', async(req,res)=>{
