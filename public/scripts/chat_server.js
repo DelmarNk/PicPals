1ee391f3034e86fae41fdfc7964cb6a970be201c
delmarNK <delmarjinku3@gmail.com>
2024-02-24 22:22:45 +0800

    created messages.ejs and a chat and messages css

--- public/scripts/chat_server.js
+++ public/scripts/chat_server.js
@@ -6,8 +6,6 @@ const messages = document.querySelector('.messages')
 
 form.addEventListener('submit', (event)=>{
     event.preventDefault()
-    console.log(chatId)
-    console.log(input.value)
     fetch(`/chat/${chatId}/`, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -22,8 +20,13 @@ form.addEventListener('submit', (event)=>{
 })
 
 socket.on('chat message', (msg)=>{
-    console.log(msg)
     const newMessage = document.createElement('li')
+    const time = document.createElement('p')
+    newMessage.className = 'my_message'
     newMessage.textContent = msg
+    time.className = 'my_message_time time'
+    time.textContent = new Date().toLocaleString()
     messages.appendChild(newMessage)
+    messages.appendChild(time)
+    window.scrollTo(0, document.body.scrollHeight) // make a msg automatically scroll to the top
 })
